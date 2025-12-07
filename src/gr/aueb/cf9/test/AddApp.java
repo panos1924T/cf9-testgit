59f89f3afec452d8c500905ec1d2eda184fb8877
pantsath <pants.ath@gmail.com>
2025-12-07 12:28:04 +0200

    feat: add add-app

---
+++ src/gr/aueb/cf9/test/AddApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf9.test;
+
+public class AddApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
